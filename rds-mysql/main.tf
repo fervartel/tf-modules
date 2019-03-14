394e451a1998691255b92d82ec14d285dbed013a
Fernando Varela <fernando.varela@thomsonreuters.com>
2019-03-14 17:15:08 -0400

    Minor

--- rds-mysql/main.tf
+++ rds-mysql/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "tf-mysql-instance" {
-  identifier                = "tf-mysql-instance-${var.rds-env}"
+  identifier                = "tf-rds-instance-${var.rds-env}"
   allocated_storage         = "${var.rds-storage}"
   storage_type              = "${var.rds-storage_type}"
   engine                    = "${var.rds-engine}"
