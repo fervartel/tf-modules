420ac1112be3cc6f3f31204a4b93a271476e3b98
Fernando Varela <fernando.varela@thomsonreuters.com>
2019-04-24 09:49:45 -0400

    resource names normalized

--- gcp-autoscaler/outputs.tf
+++ gcp-autoscaler/outputs.tf
@@ -1,6 +1,6 @@
 output load_balancer_external_ip {
   description = "The external ip address of the forwarding rule."
-  value       = "${google_compute_forwarding_rule.default.ip_address}"
+  value       = "${google_compute_forwarding_rule.main.ip_address}"
 }
 
 output available_zones {
